60ae33f71a0f548462f34938d6f5d35e7a7e8e42
Ricardo Calvo <ricardo.calvo@fitideas.co>
2022-03-29 10:18:26 -0500

    Fix v9

--- scripts/gh-pages-deploy.cjs
+++ scripts/gh-pages-deploy.cjs
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
-import { execa } from 'execa';
-import fs from 'fs';
+//import { execa } from 'execa';
+//import fs from 'fs';
+const execa = require("execa");
+const fs = require("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
